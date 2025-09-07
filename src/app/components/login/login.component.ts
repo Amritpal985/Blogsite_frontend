02dba94638b7ff532fa9cf9046061bf0fd285f5d
Nishant <nishantkarlupia149@gmail.com>
2025-09-07 19:29:26 +0530

    Login/SingUp tested.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,6 +7,8 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -19,6 +21,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
     MatCardModule,
     MatInputModule,
     MatButtonModule,
+    HttpClientModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
@@ -26,6 +29,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class LoginComponent {
   private fb = inject(FormBuilder);
   private snackBar = inject(MatSnackBar);
+  private _http = inject(HttpClient);
+  private router = inject(Router);
 
   loginForm: FormGroup = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(6)]],
@@ -50,6 +55,31 @@ export class LoginComponent {
         });
         return;
       }
+      const url = 'http://localhost:8000/users/login';
+      const body = new URLSearchParams();
+      body.set('username', this.loginForm.get('username')?.value);
+      body.set('password', this.loginForm.get('password')?.value);
+      this._http
+        .post(url, body.toString(), {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        })
+        .subscribe(
+          () => {
+            this.snackBar.open('User LoggedIn Successfully!!', 'Close', {
+              duration: 4000,
+              verticalPosition: 'top',
+              panelClass: ['snackbar-success'],
+            });
+            this.router.navigate(['']);
+          },
+          (error) => {
+            this.snackBar.open(error?.error?.detail || 'Something went wrong!!!', 'Close', {
+              duration: 4000,
+              verticalPosition: 'top',
+              panelClass: ['snackbar-error'],
+            });
+          }
+        );
     } else {
       if (this.signupForm.invalid) {
         this.snackBar.open('Some fields are invalid.', 'Close', {
@@ -59,6 +89,35 @@ export class LoginComponent {
         });
         return;
       }
+      if (
+        this.signupForm.get('password')?.value !== this.signupForm.get('confirmPassword')?.value
+      ) {
+        this.snackBar.open('Passwords do not match.', 'Close', {
+          duration: 4000,
+          verticalPosition: 'top',
+          panelClass: ['snackbar-error'],
+        });
+        return;
+      }
+      const url = 'http://localhost:8000/users/signup';
+      const body = { ...this.signupForm.value, role: 'user' };
+
+      this._http.post(url, body).subscribe(
+        () => {
+          this.snackBar.open('User created Successfully!!', 'Close', {
+            duration: 4000,
+            verticalPosition: 'top',
+            panelClass: ['snackbar-success'],
+          });
+        },
+        (error) => {
+          this.snackBar.open(error?.error?.detail || 'Something went wrong!!!', 'Close', {
+            duration: 4000,
+            verticalPosition: 'top',
+            panelClass: ['snackbar-error'],
+          });
+        }
+      );
     }
   }
 }

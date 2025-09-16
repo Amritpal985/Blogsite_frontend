df7cfd5bbd17ef996a3e5d442280a1eaa249f49d
Nishant Karlupia <nishant.karlupia@pkware.com>
2025-09-16 18:21:16 +0530

    fixup! Added `AddPost` component.

--- src/app/components/add-post/add-post.component.ts
+++ src/app/components/add-post/add-post.component.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Editor, NgxEditorModule } from 'ngx-editor';
 
 @Component({
   selector: 'app-add-post',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, NgxEditorModule, FormsModule],
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.scss'
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnDestroy{
+  editor: Editor = new Editor();
+  html = "";
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 
 }
